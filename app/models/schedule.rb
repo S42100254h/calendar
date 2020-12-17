39a501d74aad7e571abb8800d63f07902022dc8b
yuki sawabe <sawabe0v0@gmail.com>
2020-12-17 21:43:49 +0900

    add validation for Schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,7 @@
 class Schedule < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { max: 50 }
+  validates :location, presence: true, length: { max: 50 }
+  validates :description, presence: true
+  validates :date, presence: true
 end
