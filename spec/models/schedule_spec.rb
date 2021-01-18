63ac55e8e846c693dc94ee1dfa75a1b195a3d775
yuki sawabe <sawabe0v0@gmail.com>
2021-01-18 13:29:52 +0900

    normal scenario for schedule model

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '正常系テスト' do
+    context 'title, dateが入力されている' do
+      let(:schedule) { create(:schedule) }
+
+      it 'スケジュールが作られる' do
+        expect(schedule.valid?).to eq true
+      end
+    end
+  end
 end
