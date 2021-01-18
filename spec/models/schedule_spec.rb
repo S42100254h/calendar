0bd021a1478898957b02ec858bc536b46949c842
yuki sawabe <sawabe0v0@gmail.com>
2021-01-18 13:52:43 +0900

    rubocop -a

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Schedule, type: :model do
   describe '異常系テスト' do
     context 'titleが入力されていない' do
       let(:schedule) { build(:schedule, title: nil) }
-      
+
       it 'スケジュールが作られない' do
         schedule.valid?
         expect(schedule.errors.messages[:title]).to include "can't be blank"
@@ -31,19 +31,19 @@ RSpec.describe Schedule, type: :model do
     end
 
     context 'titleが51文字以上' do
-      let(:schedule) { build(:schedule, title: "a" * 51) }
+      let(:schedule) { build(:schedule, title: 'a' * 51) }
       it 'スケジュールが作られない' do
         schedule.valid?
-        expect(schedule.errors.messages[:title]).to include "is too long (maximum is 50 characters)"
+        expect(schedule.errors.messages[:title]).to include 'is too long (maximum is 50 characters)'
       end
     end
 
     context 'locationが51文字以上' do
-      let(:schedule) { build(:schedule, location: "a" * 51) }
+      let(:schedule) { build(:schedule, location: 'a' * 51) }
 
       it 'スケジュールが作られない' do
         schedule.valid?
-        expect(schedule.errors.messages[:location]).to include "is too long (maximum is 50 characters)"
+        expect(schedule.errors.messages[:location]).to include 'is too long (maximum is 50 characters)'
       end
     end
   end
