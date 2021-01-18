1ef3be9324d3b43e4e17d9b6ee7969833ed1b8fd
yuki sawabe <sawabe0v0@gmail.com>
2021-01-18 10:02:26 +0900

    add test for show action

--- spec/requests/api/v1/schedules_spec.rb
+++ spec/requests/api/v1/schedules_spec.rb
@@ -16,4 +16,33 @@ RSpec.describe 'Api::V1::Schedules', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /api/v1/schedule/:id' do
+    subject { get(api_v1_schedule_path(schedule_id)) }
+
+    context '指定したidのスケジュールが存在する場合' do
+      let(:schedule) { create(:schedule, id: 1) }
+      let(:schedule_id) { schedule.id }
+
+      it 'スケジュールの詳細を取得できる' do
+        subject
+        res = JSON.parse(response.body)
+        expect(res["id"]).to eq 1
+        expect(res["title"]).to eq schedule.title
+        expect(res["location"]).to eq schedule.location
+        expect(res["description"]).to eq schedule.description
+        expect(res["date"]).to eq schedule.date.strftime("%Y-%m-%d")
+        expect(res.keys).to eq ['id', 'title', 'location', 'description', 'date', 'user']
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context '指定したidのスケジュールが存在しない場合' do
+      let(:schedule_id) { 9999 }
+
+      it 'スケジュールの詳細を取得できない' do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
 end
