58b5c99320d78a51a5e3761a919b1ff13686ce3e
yuki sawabe <sawabe0v0@gmail.com>
2021-01-18 13:52:00 +0900

    add exception scenario

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -10,4 +10,41 @@ RSpec.describe Schedule, type: :model do
       end
     end
   end
+
+  describe '異常系テスト' do
+    context 'titleが入力されていない' do
+      let(:schedule) { build(:schedule, title: nil) }
+      
+      it 'スケジュールが作られない' do
+        schedule.valid?
+        expect(schedule.errors.messages[:title]).to include "can't be blank"
+      end
+    end
+
+    context 'dateが入力されていない' do
+      let(:schedule) { build(:schedule, date: nil) }
+
+      it 'スケジュールが作られない' do
+        schedule.valid?
+        expect(schedule.errors.messages[:date]).to include "can't be blank"
+      end
+    end
+
+    context 'titleが51文字以上' do
+      let(:schedule) { build(:schedule, title: "a" * 51) }
+      it 'スケジュールが作られない' do
+        schedule.valid?
+        expect(schedule.errors.messages[:title]).to include "is too long (maximum is 50 characters)"
+      end
+    end
+
+    context 'locationが51文字以上' do
+      let(:schedule) { build(:schedule, location: "a" * 51) }
+
+      it 'スケジュールが作られない' do
+        schedule.valid?
+        expect(schedule.errors.messages[:location]).to include "is too long (maximum is 50 characters)"
+      end
+    end
+  end
 end
