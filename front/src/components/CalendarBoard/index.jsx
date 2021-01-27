023e74717263bb50da03a14912227c82a33ab833
yuki sawabe <sawabe0v0@gmail.com>
2021-01-28 06:45:21 +0900

    create Array

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -2,43 +2,11 @@ import React from "react";
 import { GridList } from "@material-ui/core";
 import "./style.css";
 
-const calendar = [
-  "29",
-  "30",
-  "10月1日",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "23",
-  "24",
-  "25",
-  "26",
-  "27",
-  "28",
-  "29",
-  "30",
-  "31",
-  "11月1日",
-  "2",
-];
+const createCalendar = () => {
+  return Array(35).fill(0).map((_, i) => i);
+};
+
+const calendar = createCalendar();
 
 const CalendarBoard = () => {
   return (
