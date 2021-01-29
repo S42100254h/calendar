3c7e9830a65ed7282fdf9b2d13838ebcf1c163c7
yuki sawabe <sawabe0v0@gmail.com>
2021-01-29 12:20:39 +0900

    modify Navigation component

--- front/src/components/Navigation/index.jsx
+++ front/src/components/Navigation/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { IconButton, Toolbar, Typography } from "@material-ui/core";
+import ArrowBackIos from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIos from "@material-ui/icons/ArrowForwardIos";
+import DehazeIcon from "@material-ui/icons/Dehaze";
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 
 const Navigation = () => {
-  return <div>navigation</div>;
+  return (
+    <Toolbar>
+      <IconButton>
+        <DehazeIcon />
+      </IconButton>
+      <CalendarTodayIcon />
+      <Typography color="textSecondary" variant="h5" component="h1">
+        カレンダー
+      </Typography>
+      <IconButton size="small">
+        <ArrowBackIos />
+      </IconButton>
+      <IconButton size="small">
+        <ArrowForwardIos />
+      </IconButton>
+    </Toolbar>
+  );
 };
 
 export default Navigation;
