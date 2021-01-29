787276f068998982d0c5b79219ec92c4be62f715
yuki sawabe <sawabe0v0@gmail.com>
2021-01-29 12:15:30 +0900

    create Navigation/container.jsx

---
+++ front/src/components/Navigation/container.jsx
@@ -0,0 +1,10 @@
+import Navigation from "./index";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+const mergeProps = (stateProps, dispatchProps) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Navigation);
