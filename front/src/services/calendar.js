55440e0350a695465201fda1fd6cc6b1cc94d66f
yuki sawabe <sawabe0v0@gmail.com>
2021-01-29 13:56:01 +0900

    create getNextMonth, getPriviousMonth method

--- front/src/services/calendar.js
+++ front/src/services/calendar.js
@@ -1,10 +1,11 @@
 import dayjs from "dayjs";
 
+// 受け取ったpropsをdayjsインスタンスに変更
 export const getMonth = ({ year, month }) => {
   return dayjs(`${year}-${month}`);
 };
 
-const createCalendar = (month) => {
+export const createCalendar = (month) => {
   // 今月の最初の日を追加
   const firstDay = getMonth(month);
   // 最初の日の曜日のindexを取得
@@ -17,4 +18,18 @@ const createCalendar = (month) => {
   });
 };
 
-export default createCalendar;
+// 受け取ったdayjsインスタンスのフォーマットを変更
+export const formatMonth = (day) => ({
+  month: day.month() + 1,
+  year: day.year(),
+});
+
+export const getNextMonth = (month) => {
+  const day = getMonth(month).add(1, "month");
+  return formatMonth(day);
+};
+
+export const getPriviousMonth = (month) => {
+  const day = getMonth(month).add(-1, "month");
+  return formatMonth(day);
+};
