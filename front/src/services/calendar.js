ebec0233d0222d6697b821f509b731f9365ab3ec
yuki sawabe <sawabe0v0@gmail.com>
2021-01-29 11:30:05 +0900

    modify create calendar logic

--- front/src/services/calendar.js
+++ front/src/services/calendar.js
@@ -1,8 +1,8 @@
 import dayjs from "dayjs";
 
-const createCalendar = () => {
+const createCalendar = (month) => {
   // 今月の最初の日を追加
-  const firstDay = dayjs().startOf("month");
+  const firstDay = getMonth(month);
   // 最初の日の曜日のindexを取得
   const firstDayIndex = firstDay.day();
 
@@ -11,6 +11,10 @@ const createCalendar = () => {
     const day = firstDay.add(diffFromDay, "day");
     return day;
   });
+};
+
+export const getMonth = ({year, month}) => {
+  return dayjs(`${year}-${month}`);
 };
 
 export default createCalendar;
