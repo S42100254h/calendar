629156eadda7100c9d6ffefbc889e6cc3380caf0
yuki sawabe <sawabe0v0@gmail.com>
2021-01-30 15:50:29 +0900

    create base of AddScheduleDialog/container

---
+++ front/src/components/AddScheduleDialog/container.jsx
@@ -0,0 +1,6 @@
+import AddScheduleDialog from "./index";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps)(AddScheduleDialog);
