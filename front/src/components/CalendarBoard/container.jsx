d9c6404c372e4e8e675ca2e58a90800cd6ce76ca
yuki sawabe <sawabe0v0@gmail.com>
2021-02-01 18:05:15 +0900

    display selected date

--- front/src/components/CalendarBoard/container.jsx
+++ front/src/components/CalendarBoard/container.jsx
@@ -1,13 +1,14 @@
 import { connect } from "react-redux";
 import { createCalendar } from "../../services/calendar";
 import CalendarBoard from "./index";
-import { addScheduleOpenDialog } from "../../redux/addSchedule/actions";
+import { addScheduleOpenDialog, addScheduleSetValue } from "../../redux/addSchedule/actions";
 
 const mapStateToProps = (state) => ({ calendar: state.calendar });
 
 const mapDispatchToProps = (dispatch) => ({
-  openAddScheduleDialog: () => {
+  openAddScheduleDialog: (d) => {
     dispatch(addScheduleOpenDialog());
+    dispatch(addScheduleSetValue({ date: d }));
   },
 });
 
