92378ed0c683b6dc14e92d7eccfbe08d0298d989
yuki sawabe <sawabe0v0@gmail.com>
2021-02-02 06:50:20 +0900

    add test for redux/addSchedule/reducer

---
+++ front/src/__test__/redux/addSchedule/reducer.spec.js
@@ -0,0 +1,42 @@
+import addScheduleReducer from "../../../redux/addSchedule/reducer";
+import { addScheduleSetValue, addScheduleOpenDialog, addScheduleCloseDialog } from "../../../redux/addSchedule/actions";
+import dayjs from "dayjs";
+
+describe("addScheduleReducerのテスト", () => {
+
+  const init = {
+    form: {
+      title: "test_title",
+      description: "test_description",
+      date: dayjs(),
+      location: "test_location",
+    },
+    isDialogOpen: false,
+  };
+
+  it("action.type === ADD_SCHEDULE_SET_VALUEの時のテスト", () => {
+    const dummy = { title: "ダミータイトル" };
+    const action = addScheduleSetValue(dummy);
+    const newState = addScheduleReducer(init, action);
+
+    expect(newState.form.title).toStrictEqual(dummy.title);
+    expect(newState.form.description).toStrictEqual(init.form.description);
+    expect(newState.form.date).toStrictEqual(init.form.date);
+    expect(newState.form.location).toStrictEqual(init.form.location);
+  });
+
+  it("action.type === ADD_SCHEDULE_OPEN_DIALOGの時のテスト", () => {
+    const action = addScheduleOpenDialog();
+    const newState = addScheduleReducer(init, action);
+
+    expect(newState.isDialogOpen).toStrictEqual(true);
+    expect(newState.form).toStrictEqual(init.form);
+  });
+
+  it("action.type === ADD_SCHEDULE_CLOSE_DIALOGの時のテスト", () => {
+    const action = addScheduleCloseDialog();
+    const newState = addScheduleReducer(init, action);
+    
+    expect(newState.isDialogOpen).toStrictEqual(init.isDialogOpen);
+  });
+});
