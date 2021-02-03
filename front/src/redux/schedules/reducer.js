684dcf69edfb66f8ba4a334e2e37213742aed580
yuki sawabe <sawabe0v0@gmail.com>
2021-02-03 18:53:33 +0900

    ./node_modules/.bin/eslint src --fix

--- front/src/redux/schedules/reducer.js
+++ front/src/redux/schedules/reducer.js
@@ -1,20 +1,19 @@
-import { PlaylistAddOutlined } from "@material-ui/icons";
 import { SCHEDULES_ADD_ITEM } from "./actions";
 
 const init = {
   items: [],
-  isLoading: false
+  isLoading: false,
 };
 
 const schedulesReducer = (state = init, action) => {
   switch (action.type) {
-    case SCHEDULES_ADD_ITEM:
-      return {
-        ...state,
-        items: [...state.items, { ...payload }]
-      };  
-    default:
-      return state;
+  case SCHEDULES_ADD_ITEM:
+    return {
+      ...state,
+      items: [...state.items, { ...action.payload }],
+    };
+  default:
+    return state;
   }
 };
 
