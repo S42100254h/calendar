eca2b550d89d07a7eb1adb8db4efc5c54f4f4a64
yuki sawabe <sawabe0v0@gmail.com>
2021-02-06 10:31:59 +0900

    create currentScheduleReducer

---
+++ front/src/redux/currentSchedule/reducer.js
@@ -0,0 +1,25 @@
+import {
+  CURRENT_SCHEDULE_SET_ITEM,
+  CURRENT_SCHEDULE_OPEN_DIALOG,
+  CURRENT_SCHEDULE_CLOSE_DIALOG
+} from "./actions";
+
+const init = {
+  items: null,
+  isDialogOpen: false
+};
+
+const currentScheduleReducer = (state = init, action) => {
+  switch (action.type) {
+    case CURRENT_SCHEDULE_SET_ITEM:
+      return { ...state, item: action.payload };
+    case CURRENT_SCHEDULE_OPEN_DIALOG:
+      return { ...state, isDialogOpen: true };
+    case CURRENT_SCHEDULE_CLOSE_DIALOG:
+      return { ...state, isDialogOpen: false };
+    default:
+      return state;      
+  };
+};
+
+export default currentScheduleReducer;
