30b317b93cf7bc84987c66efcdff1a3c20eaf245
yuki sawabe <sawabe0v0@gmail.com>
2021-02-08 19:12:06 +0900

    create actionCreator to hit Rails API

--- front/src/redux/schedules/actions.js
+++ front/src/redux/schedules/actions.js
@@ -1,6 +1,17 @@
 export const SCHEDULES_ADD_ITEM = "SCHEDULES_ADD_ITEM";
+export const SCHEDULE_FETCH_ITEM = "SCHEDULE_FETCH_ITEM";
+export const SCHEDULE_SET_LOADING = "SCHEDULE_SET_LOADING";
 
 export const schedulesAddItem = (payload) => ({
   type: SCHEDULES_ADD_ITEM,
   payload,
+});
+
+export const schedulesFetchItem = (payload) => ({
+  type: SCHEDULE_FETCH_ITEM,
+  payload,
+});
+
+export const schedulesSetLoading = () => ({
+  type: SCHEDULE_SET_LOADING
 });
