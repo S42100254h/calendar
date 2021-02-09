7980b604214c9afb24a9711c7436d65158fda75f
yuki sawabe <sawabe0v0@gmail.com>
2021-02-09 18:30:27 +0900

    modify application.rb to setup CORS

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,17 @@ module Calendar
     config.load_defaults 5.2
     config.middleware.use ActionDispatch::Flash
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "localhost:3000"
+
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete],
+          credentials: true
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
