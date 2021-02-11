f75cedd001041c46014bd5e32a2f2eea2b00ba16
yuki sawabe <sawabe0v0@gmail.com>
2021-02-11 19:07:23 +0900

    create actionCreator for async

--- front/src/redux/schedules/actions.js
+++ front/src/redux/schedules/actions.js
@@ -2,6 +2,8 @@ export const SCHEDULES_ADD_ITEM = "SCHEDULES_ADD_ITEM";
 export const SCHEDULE_FETCH_ITEM = "SCHEDULE_FETCH_ITEM";
 export const SCHEDULE_SET_LOADING = "SCHEDULE_SET_LOADING";
 export const SCHEDULE_DELETE_ITEM = "SCHEDULE_DELETE_ITEM";
+export const SCHEDULE_ASYNC_FAILURE = "SCHEDULE_ASYNC_FAILURE";
+export const SCHEDULE_RESET_ERROR = "SCHEDULE_RESET_ERROR";
 
 export const schedulesAddItem = (payload) => ({
   type: SCHEDULES_ADD_ITEM,
@@ -20,4 +22,13 @@ export const schedulesSetLoading = () => ({
 export const schedulesDeleteItem = (payload) => ({
   type: SCHEDULE_DELETE_ITEM,
   payload,
+});
+
+export const schedulesAsyncFailure = (error) => ({
+  type: SCHEDULE_ASYNC_FAILURE,
+  error,
+});
+
+export const schedulesResetError = () => ({
+  type: SCHEDULE_RESET_ERROR,
 });
