cec8e79bd52d3161c4c489fc164139625924bffc
yuki sawabe <sawabe0v0@gmail.com>
2021-02-11 14:35:18 +0900

    add test for redux/schedules/actions

--- front/src/__test__/redux/schedules/actions.spec.js
+++ front/src/__test__/redux/schedules/actions.spec.js
@@ -1,4 +1,12 @@
-import { SCHEDULES_ADD_ITEM, schedulesAddItem } from "../../../redux/schedules/actions";
+import { SCHEDULES_ADD_ITEM,
+  SCHEDULE_FETCH_ITEM,
+  SCHEDULE_SET_LOADING,
+  SCHEDULE_DELETE_ITEM,
+  schedulesAddItem,
+  schedulesFetchItem,
+  schedulesSetLoading,
+  schedulesDeleteItem,
+} from "../../../redux/schedules/actions";
 
 describe("actions.jsのテスト", () => {
   it("schedulesAddItemのテスト", () => {
@@ -8,7 +16,41 @@ describe("actions.jsのテスト", () => {
     expect(action).toStrictEqual(
       {
         type: SCHEDULES_ADD_ITEM,
-        payload: dummy
+        payload: dummy,
+      }
+    );
+  });
+
+  it("schedulesFetchItemのテスト", () => {
+    const dummy = { month: 2, year: 2021 };
+    const action = schedulesFetchItem(dummy);
+
+    expect(action).toStrictEqual(
+      {
+        type: SCHEDULE_FETCH_ITEM,
+        payload: dummy,
+      }
+    );
+  });
+
+  it("schedulesSetLoadingのテスト", () => {
+    const action = schedulesSetLoading();
+
+    expect(action).toStrictEqual(
+      {
+        type: SCHEDULE_SET_LOADING,
+      }
+    );
+  });
+
+  it("schedulesDeleteItemのテスト", () => {
+    const dummy = { month: 2, year: 2021 };
+    const action = schedulesDeleteItem(dummy);
+
+    expect(action).toStrictEqual(
+      {
+        type: SCHEDULE_DELETE_ITEM,
+        payload: dummy,
       }
     );
   });
