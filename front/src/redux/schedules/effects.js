9178f8a1c484b0d1527d67f1aee57e50dfa3ec22
yuki sawabe <sawabe0v0@gmail.com>
2021-02-11 18:58:54 +0900

    add try&catch to asyncActionCreator

--- front/src/redux/schedules/effects.js
+++ front/src/redux/schedules/effects.js
@@ -10,29 +10,41 @@ import { formatSchedule } from "../../services/schedule";
 export const asyncSchedulesFetchItem = ({ month, year }) => async (dispatch) => {
   dispatch(schedulesSetLoading());
 
-  const result = await get(`schedules?month=${month}&year=${year}`);
-  const formatedSchedule = result.map((r) => formatSchedule(r));
+  try {
+    const result = await get(`schedules?month=${month}&year=${year}`);
+    const formatedSchedule = result.map((r) => formatSchedule(r));
 
-  dispatch(schedulesFetchItem(formatedSchedule));
+    dispatch(schedulesFetchItem(formatedSchedule));
+  } catch(err) {
+    console.error(err);
+  }
 };
 
 export const asyncSchedulesAddItem = (schedule) => async (dispatch) => {
   dispatch(schedulesSetLoading());
 
-  const body = { ...schedule, date: schedule.date.toISOString() };
-  const result = await post("schedules", body);
+  try {
+    const body = { ...schedule, date: schedule.date.toISOString() };
+    const result = await post("schedules", body);
 
-  const newSchedule = formatSchedule(result);
-  dispatch(schedulesAddItem(newSchedule));
+    const newSchedule = formatSchedule(result);
+    dispatch(schedulesAddItem(newSchedule));
+  } catch(err) {
+    console.error(err);
+  }
 };
 
 export const asyncSchedulesDeleteItem = (id) => async (dispatch, getState) => {
   dispatch(schedulesSetLoading());
   const currentSchedules = getState().schedules.items;
 
-  await deleteRequest(`schedules/${id}`);
+  try {
+    await deleteRequest(`schedules/${id}`);
 
-  // 成功したらローカルのstateを削除
-  const newSchedules = currentSchedules.filter((s) => s.id !== id);
-  dispatch(schedulesDeleteItem(newSchedules));
+    // 成功したらローカルのstateを削除
+    const newSchedules = currentSchedules.filter((s) => s.id !== id);
+    dispatch(schedulesDeleteItem(newSchedules));
+  } catch(err) {
+    console.error(err);
+  }
 };
