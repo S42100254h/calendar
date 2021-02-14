7124d3991b454eb8483ba8b4dd2f0eb1d1ae2174
yuki sawabe <sawabe0v0@gmail.com>
2021-02-14 15:10:05 +0900

    update Login/container

--- front/src/components/Login/container.jsx
+++ front/src/components/Login/container.jsx
@@ -1,11 +1,30 @@
 import { connect } from "react-redux";
 import Login from "./index";
 import loginSetValue from "../../redux/login/actions";
+import { asyncLoginSetValue } from "../../redux/login/effects";
+
+const mapStateToProps = (state) => ({ loginValue: state.login });
 
 const mapDispatchToProps = (dispatch) => ({
   setLogin: (value) => {
     dispatch(loginSetValue(value));
   },
+  login: (loginValue) => {
+    dispatch(asyncLoginSetValue());
+  },
 });
 
-export default connect(mapDispatchToProps)(Login);
+const mergeProps = (stateProps, dispatchProps) => {
+  const { loginValue: { form: loginValue }} = stateProps;
+  const { login } = dispatchProps;
+
+  return {
+    ...stateProps,
+    ...dispatchProps,
+    login: () => {
+      dispatchProps.login(loginValue);
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Login);
