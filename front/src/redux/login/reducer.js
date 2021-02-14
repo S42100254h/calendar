a6afef56375a40c5dace6a3a3ce366dc41991f16
yuki sawabe <sawabe0v0@gmail.com>
2021-02-14 14:19:46 +0900

    create loginReducer

---
+++ front/src/redux/login/reducer.js
@@ -0,0 +1,21 @@
+import { loginSetValue } from "./actions";
+
+const init = {
+  form: {
+    name: "",
+    email: "",
+    password: "",
+  },
+  isStartEdit: false,
+};
+
+const loginReducer = (state = init, action) => {
+  switch (action.type) {
+    case loginSetValue:
+      return { ...state, form: {...state.form, ...action.payload } };
+    default:
+      return state;
+  }
+};
+
+export default loginReducer;
