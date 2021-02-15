db65321863c32b808288f2ad8420787d59eb8928
yuki sawabe <sawabe0v0@gmail.com>
2021-02-15 19:22:19 +0900

    modify App.js & index.js to user Provider in index.js

--- front/src/index.js
+++ front/src/index.js
@@ -3,17 +3,26 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./redux/rootReducer";
+
 import App from "./App";
-import Login from "./components/Login";
+import Login from "./components/Login/container";
 
 dayjs.locale("ja");
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Route exact path="/" component={App} />
-      <Route exact path="/login" component={Login} />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
