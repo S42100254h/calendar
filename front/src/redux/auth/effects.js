0081f2a185bc41fbb38ef47b89c7d3103b3e1453
yuki sawabe <sawabe0v0@gmail.com>
2021-02-27 16:48:42 +0900

    add asyncAuthSignUp func

--- front/src/redux/auth/effects.js
+++ front/src/redux/auth/effects.js
@@ -1,4 +1,4 @@
-import { authSignIn } from "./actions";
+import { authSignIn, authSignUp } from "./actions";
 import { post } from "../../services/api";
 
 const header = {
@@ -12,4 +12,11 @@ export const asyncAuthSignIn = (value) => async (dispatch) => {
   const result = await post("auth/sign_in", body, header);
 
   dispatch(authSignIn(result));
+};
+
+export const asyncAuthSignUp = (value) => async (dispatch) => {
+  const body = { ...value };
+  const result = await post("auth", body, header);
+
+  dispatch(authSignUp(result));
 };
