c1309c332eb42b0b25e73a25f4d47911f44150ee
yuki sawabe <sawabe0v0@gmail.com>
2021-02-28 15:10:17 +0900

    add authSignOut action

--- front/src/redux/auth/actions.js
+++ front/src/redux/auth/actions.js
@@ -1,6 +1,7 @@
 export const AUTH_SET_VALUE = "AUTH_SET_VALUE";
 export const AUTH_SIGN_IN = "AUTH_SIGN_IN";
 export const AUTH_SIGN_UP = "AUTH_SIGN_UP";
+export const AUTH_SIGN_OUT = "AUTH_SIGN_OUT";
 
 export const authSetValue = (payload) => ({
   type: AUTH_SET_VALUE,
@@ -15,4 +16,8 @@ export const authSignIn = (payload) => ({
 export const authSignUp = (payload) => ({
   type: AUTH_SIGN_UP,
   payload,
+});
+
+export const authSignOut = () => ({
+  type: AUTH_SIGN_OUT,
 });
