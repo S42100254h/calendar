e0a2c1bae3e84386c0353dcc0d0ecb89cf8d826c
yuki sawabe <sawabe0v0@gmail.com>
2021-02-28 18:02:44 +0900

    create test for signUp, signOut actions

--- front/src/__test__/redux/Auth/actions.spec.js
+++ front/src/__test__/redux/Auth/actions.spec.js
@@ -1,8 +1,12 @@
 import {
   AUTH_SET_VALUE,
   AUTH_SIGN_IN,
+  AUTH_SIGN_UP,
+  AUTH_SIGN_OUT,
   authSetValue,
   authSignIn,
+  authSignUp,
+  authSignOut,
 } from "../../../redux/auth/actions";
 
 describe("actions.jsのテスト", () => {
@@ -24,9 +28,31 @@ describe("actions.jsのテスト", () => {
 
     expect(action).toStrictEqual(
       {
-        type:   AUTH_SIGN_IN,
+        type: AUTH_SIGN_IN,
         payload: dummy
       }
     );
+  });
+
+  it("authSignUpのテスト", () => {
+    const dummy = { name: "neko", email: "nekoneko@gmail.com", password: "nekowanko" };
+    const action = authSignUp(dummy);
+
+    expect(action).toStrictEqual(
+      {
+        type: AUTH_SIGN_UP,
+        payload: dummy
+      }
+    )
+  });
+
+  it("authSignOutのテスト", () => {
+    const action = authSignOut();
+
+    expect(action).toStrictEqual(
+      {
+        type: AUTH_SIGN_OUT
+      }
+    )
   });
 });
