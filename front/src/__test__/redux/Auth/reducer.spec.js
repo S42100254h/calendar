5f78d75e1574fc05a49a1b56da228c8efb7a311b
yuki sawabe <sawabe0v0@gmail.com>
2021-02-28 18:25:48 +0900

    create test for authReducer

--- front/src/__test__/redux/Auth/reducer.spec.js
+++ front/src/__test__/redux/Auth/reducer.spec.js
@@ -1,5 +1,5 @@
 import authReducer from "../../../redux/auth/reducer";
-import { authSetValue, authSignIn } from "../../../redux/auth/actions";
+import { authSetValue, authSignIn, authSignUp, authSignOut } from "../../../redux/auth/actions";
 import Faker from "faker";
 
 const init = {
@@ -20,17 +20,36 @@ describe("authReducerのテスト", () => {
     expect(newState.form.name).toStrictEqual(dummy.name);
   });
 
-  it("action.type === authSignInの時のテスト", () => {
+  it("action.type === AUTH_SIGN_INの時のテスト", () => {
     const dummy = {
-      name: Faker.internet.userName(),
       email: Faker.internet.email(),
       password: Faker.internet.password()
     };
     const action = authSignIn(dummy);
     const newState = authReducer(init, action);
 
+    expect(newState.form.email).toStrictEqual(dummy.email);
+    expect(newState.form.password).toStrictEqual(dummy.password);
+  });
+
+  it("action.type === AUTH_SIGN_UPの時のテスト", () => {
+    const dummy = {
+      name: Faker.internet.userName(),
+      email: Faker.internet.email(),
+      password: Faker.internet.password()
+    };
+    const action = authSignUp(dummy);
+    const newState = authReducer(init, action);
+
     expect(newState.form.name).toStrictEqual(dummy.name);
     expect(newState.form.email).toStrictEqual(dummy.email);
     expect(newState.form.password).toStrictEqual(dummy.password);
+  });
+
+  it("action.type === AUTH_SIGN_OUTの時のテスト", () => {
+    const action = authSignOut();
+    const newState = authReducer(init, action);
+
+    expect(newState).toStrictEqual(init);
   });
 });
